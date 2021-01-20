2fa65c1d5e7e6fe6de755d9cc89097b2220ee4c2
AhmedYasin487 <coolahmed21@example.com>
2021-01-19 20:39:12 -0800

    new

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dotescrow.wsgi --log-file -
--- source/settings.py
+++ source/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '%su!^b#%49!1a%emc^ql#eh)+6e2gdc(@4d#=p18nvy4)@5eo&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','31.220.62.62']
+ALLOWED_HOSTS = ['glacial-tundra-50249.herokuapp.com','31.220.62.62']
 
 
 # Application definition
