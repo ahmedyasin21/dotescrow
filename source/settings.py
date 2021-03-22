0b4f4f1d93704cb9535e0ec999dcad3a9a0fc992
Hamza <coolahmed21@gmail.com>
2021-03-23 02:16:35 +0500

    add

--- source/settings.py
+++ source/settings.py
@@ -179,7 +179,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'source.storage.WhiteNoiseStaticFilesStorage'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
